bc74e10c99cbc764f921c35d15adb020ab750223
summer <summerthe43@gmail.com>
2022-02-07 22:55:43 +0530

    added nested router

--- config/settings/base.py
+++ config/settings/base.py
@@ -227,6 +227,7 @@ REST_FRAMEWORK = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
+CORS_ALLOW_ALL_ORIGINS = True
 # Your stuff...
 # ------------------------------------------------------------------------------
 
